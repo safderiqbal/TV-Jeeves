7b3a9d8223fb1d2bad7510d04d92861380e90f8c
Safder Iqbal <safder.iqbal@gmail.com>
2016-10-30 05:54:11 +0000

    shhhhhh Dan's sleeping

---
+++ bot/TVJeeves.Core/Entities/TVShow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVJeeves.Core.Entities
+{
+    public class TVShow
+    {
+        public int eventid { get; set; }
+
+        public int channelid { get; set; }
+
+        public DateTime date { get; set; }
+
+        public DateTime start { get; set; }
+
+        public int dur { get; set; }
+
+        public string title { get; set; }
+
+        public string shortDesc { get; set; }
+
+        public int genre { get; set; }
+
+        public int subgenre { get; set; }
+
+        public string scheduleStatus { get; set; }
+    }
+}
